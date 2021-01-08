cdef7cbc2c1c0b5f9484b89e5aed3d5b3ae296ca
Muhammad Adan Sarfraz <ali@Addis-MacBook-Pro.local>
2021-01-08 12:42:04 +0500

    https://github.com/anoweb123/QCOLLECTIONPK.COM.git

---
+++ files/header.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+
+<body>
+    <div>
+<!-- top navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-color py-0">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a href="">
+            <i class="fa fa-twitter fa-lg pr-3" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="">
+            <i class="fa fa-facebook fa-lg pr-3" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li class="nav-item dropdown">
+        <a href="">
+            <i class="fa fa-youtube fa-lg pr-3" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="">
+            <i class="fa fa-instagram fa-lg pr-3" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="">
+            <i class="fa fa-envelope pr-3" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="">
+            <small class="pr-3">+92 348 1112356</small>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="">
+            <small>30 DAYS HAPPINESS GUARANTEE</small>
+        </a>
+      </li>
+    </ul>
+    <div class="form-inline my-2 my-lg-0">
+    <ul class="navbar-nav small">
+    <li class="nav-item">
+        <a href="">
+            <p class="pr-4 pt-2">HELP</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="">
+            <p class="pt-2">RETURN & EXCHANGE</p>
+        </a>
+      </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- middle navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-color sticky-top">
+  <a class="navbar-brand ml-3" href="#">
+    <img width="80px" height="50px" src="" alt="LOGO">
+  </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <form class="input-group md-form form-sm form-2 pl-3">
+        <input class="form-control my-0 py-1 lime-border" type="text" placeholder="What are you looking for..." aria-label="Search">
+        <div class="input-group-append">
+            <span class="input-group-text  search_icon" id="basic-text1"><i class="fa fa-search"
+                aria-hidden="true"></i></span>
+        </div>
+    </form>
+    <div class="form-inline my-2 my-lg-0 ml-5 sidebar-social cart-button">
+    <ul>
+        <li>
+            <a href="" title="Cart" rel="nofollow">
+                <i class="fa fa-shopping-basket"></i><span>cart</span>
+            </a>
+            <div class="cart-dropdown">
+                <div class="font-weight-bold head-dropdown">Shopping Cart <small>(0)</small></div>
+                <hr>
+                <div class="empty-cart text-center w-100">
+                        <div class="">
+                            <div class="cart-icon pt-2">
+                                <i class="fa fa-shopping-basket text-dark" style="font-size: 50px; height:50px; width:50px" aria-hidden="true"></i>
+                                <p class="font-weight-bold mt-3" style="letter-spacing: 1.5px;">Your Cart is Empty</p>
+                                <p class="font-weight-bold mt-3"  style="letter-spacing: 1.5px;">Continue to Shop</p>
+                                
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </li>
+    </ul>
+    
+    </div>
+  </div>
+</nav>
+
+<!-- bottom Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-color">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active btn-special">
+        <a href="">
+            <i class="fa fa-twitter fa-lg pr-3" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="">
+            <i class="fa fa-facebook fa-lg pr-3" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li class="nav-item dropdown">
+        <a href="">
+            <i class="fa fa-youtube fa-lg pr-3" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="">
+            <i class="fa fa-instagram fa-lg pr-3" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="">
+            <i class="fa fa-envelope pr-3" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="">
+            <small class="pr-3">+92 348 1112356</small>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="">
+            <small>30 DAYS HAPPINESS GUARANTEE</small>
+        </a>
+      </li>
+    </ul>
+    <div class="form-inline my-2 my-lg-0">
+    <ul class="navbar-nav small">
+    <li class="nav-item">
+        <a href="">
+            <p class="pr-4 pt-2">HELP</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="">
+            <p class="pt-2">RETURN & EXCHANGE</p>
+        </a>
+      </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</div>
+</body>
+</html>
